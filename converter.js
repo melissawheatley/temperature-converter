ca10c4a26e86e56870fdd53d2819e6da4512827e
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-02-16 15:55:59 -0600

    add functions for converting and for keypress. still need to add classes for color

--- converter.js
+++ converter.js
@@ -3,41 +3,55 @@
 console.log("converter js says hello");
 
 var input = document.getElementById("inputTemp");
-// console.log("input", input);
+console.log("input", input);
 let clear = document.getElementById("clear");
 var output = document.getElementById("convertedTemp");
-let button = document.getElementById("");
+let button = document.getElementById("convertBtn");
+let celsius = document.getElementById("celsiusCheck");
+let fahrenheit = document.getElementById("fahrenheitCheck")
 
+
+// EVENT LISTENERS 
 clear.addEventListener("click", clearDiv);
-input.addEventListener("click", converter)   
+button.addEventListener("click", determineConverter);  
+input.addEventListener("keydown", pressEnter);
 
+// FUNCTIONS
 function clearDiv() {
     input.value= " ";
+    output.innerHTML = " ";
 }
 
-
-// Create a button that, when clicked, displays the converted temperature.
+function pressEnter(clickEvent){
+    if(clickEvent.keyCode === 13){
+        determineConverter(clickEvent);
+    }
+}
 // Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
 
-function toCelsius () {
-
-}
-
 function toFahrenheit () {
-
+    var fahrTemp = (input.value * 1.8) + 32;
+    console.log(fahrTemp);
+    output.innerHTML = `${fahrTemp.toFixed()} &deg; F`
 }
 
-// // Get a reference to the button element in the DOM
-// var button = document.getElementById("converter");
+function toCelsius () {
+    var celsiusTemp = (input.value - 32) * (5/9);
+    console.log(celsiusTemp);
+    output.innerHTML = `<p>${input.value}&deg; F converts to: </p><h2> ${celsiusTemp.toFixed()} &deg; C</h2>`
+}
 
-// // This function should determine which conversion should
-// // happen based on which radio button is selected.
-// function determineConverter (clickEvent) {
-//   console.log("event", clickEvent);
-// }
+function determineConverter (clickEvent) {
+  console.log("event", clickEvent);
+  if(celsius.checked === true){
+    toCelsius();
+  }else if(fahrenheit.checked === true){
+    toFahrenheit();
+  }else{
+    output.innerHTML = "error";    
+  }
+}
 
-// // Assign a function to be executed when the button is clicked
-// button.addEventListener("click", determineConverter);
 
 // If the temperature is greater than 90F/32C the color of the converted temperature should be red.
 // If the temperature is less than 32F/0C the color of the converted temperature should be blue.
